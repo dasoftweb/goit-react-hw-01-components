483c8e79a5c321d68a201ce46ac052434025e1e2
Klirik <dasoftweb@gmail.com>
2021-04-17 09:59:26 +0300

    readme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const imageUrl =
+//   'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
+// const price = 10.99;
+
+// const product = (
+//   <div>
+//     <img src={imageUrl} alt="Tacos With Lime" width="640" />
+//     <h2>Tacos With Lime</h2>
+//     <p>Price: {price}$</p>
+//     <button type="button">Add to cart</button>
+//   </div>
+// );
+
+// ReactDOM.render(product, document.getElementById('root'))
